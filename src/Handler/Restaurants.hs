9da50357c8c7d7bba1f33114171d5c9073733359
ciukstar <ciukstar@yahoo.com>
2024-10-16 04:33:27 +0300

    Add cuisine chips

--- src/Handler/Restaurants.hs
+++ src/Handler/Restaurants.hs
@@ -12,19 +12,22 @@ module Handler.Restaurants
   ) where
 
 import Control.Applicative ((<|>))
-import qualified Control.Lens as L ( (^?), (^.) )
+import qualified Control.Lens as L ( (^?) )
 import Control.Monad.IO.Class (liftIO)
 
 import Data.Aeson
-    ( FromJSON (parseJSON), withObject, ToJSON (toJSON), object, encode
+    ( FromJSON (parseJSON), ToJSON (toJSON), withObject, object, encode
     , (.:), (.:?), (.=)
     )
 import qualified Data.Aeson as A (Value) 
-import Data.Aeson.Lens (key, AsValue (_String), nth)
+import Data.Aeson.Lens (key)
 import Data.Aeson.Types (Parser, parseMaybe)
-import Data.Maybe (catMaybes, fromMaybe, isJust)
+import Data.List (sortBy)
+import qualified Data.List.Safe as LS (head)
+import Data.Maybe (catMaybes, fromMaybe, isJust, mapMaybe)
+import qualified Data.Set as S (Set, fromList, size)
 import Data.Text (Text, unpack, intercalate)
-import Data.Text as T (null)
+import Data.Text as T (splitOn)
 import Data.Text.Lazy.Encoding (decodeUtf8)
 
 import Foundation
@@ -55,7 +58,7 @@ import Settings.StaticFiles
     , img_compass_needle_svg
     )
 
-import Text.Hamlet (Html)
+import Text.Hamlet (Html, shamlet)
 import Text.Shakespeare.Text (st)
 
 import Yesod.Core
@@ -66,11 +69,11 @@ import Yesod.Core
 import Yesod.Core.Widget (setTitleI)
 import Yesod.Form.Input (runInputGet, ireq, iopt)
 import Yesod.Form.Fields (intField, textField)
-import Data.List (sortBy)
+import Text.Blaze.Renderer.Text (renderMarkup)
 
 
 getRestaurantR :: Handler Html
-getRestaurantR = do
+getRestaurantR = undefined {-- do
 
     rid <- runInputGet $ ireq intField "rid" :: Handler Int
     
@@ -97,55 +100,58 @@ getRestaurantR = do
     defaultLayout $ do
         setTitleI MsgRestaurant
         
-        $(widgetFile "restaurants/restaurant")
+        $(widgetFile "restaurants/restaurant") --}
 
 
 getRestaurantsR :: Handler TypedContent
 getRestaurantsR = do
 
     q <- runInputGet $ iopt textField "q"
+    cuisine <- runInputGet $ iopt textField "cuisine"
     
-    let queryDefault = case q of
-          Nothing -> [st|
-                       [out:json];
+    let query = renderMarkup
+            [shamlet|
+                [out:json];
 
-                       rel["ISO3166-2"="KZ-75"] -> .rel;
-                       .rel map_to_area -> .city;
+                rel["ISO3166-2"="KZ-75"] -> .rel;
+                .rel map_to_area -> .city;
 
-                       node["amenity"="restaurant"]["name"](area.city);
+                node["amenity"="restaurant"]["name"](area.city) -> ._;
+                $maybe x <- q
+                  node._["name"~"#{x}",i] -> ._;
+                $maybe x <- cuisine
+                  node._["cuisine"~"#{x}",i] -> ._;
 
-                       out body;
-                   |]
-          
-          Just x -> [st|
-                       [out:json];
+                ._ out body;
+            |]
 
-                       rel["ISO3166-2"="KZ-75"] -> .rel;
-                       .rel map_to_area -> .city;
-
-                       node["amenity"="restaurant"]["name"](area.city) -> .named;
-                       node.named["name"~"#{x}",i];
-
-                       out body;
-                   |]
-
+    liftIO $ print query
+            
     msgr <- getMessageRender
     msgs <- getMessages
 
-    r <- liftIO $ post (unpack overpass) ["data" := queryDefault]
+    r <- liftIO $ post (unpack overpass) ["data" := query]
             
     let restaurants :: [Restaurant]
-        restaurants = sortBy (\a b -> compare (name a) (name b)) . fromMaybe []
+        restaurants = sortBy (\a b -> compare (restaurantName a) (restaurantName b)) . fromMaybe []
             $ parseMaybe parseJSON =<< (r L.^? WL.responseBody . key "elements")
+
+    let cuisines :: S.Set Text
+        cuisines = S.fromList $ concat $ mapMaybe restaurantCuisine restaurants
     
     selectRep $ do
             
         provideRep $ defaultLayout $ do
             setTitleI MsgRestaurants
 
+            idFormSearch <- newIdent
             idFieldSearch <- newIdent
             idInputSearch <- newIdent
             idListSearchResults <- newIdent
+
+            idMainSection <- newIdent
+
+            idDetailsCuisine <- newIdent
 
             idDialogDetails <- newIdent
             idDialogDetailsTitle <- newIdent
@@ -183,7 +189,7 @@ instance FromJSON Restaurant where
             <*> e .: "lon"
             <*> t .: "name"
             <*> t .:? "description"
-            <*> t .:? "cuisine"
+            <*> ( (splitOn ";" <$>) <$> (t .:? "cuisine"))
             <*> t .:? "opening_hours"
             <*> ( (\a b c -> intercalate ", " (catMaybes [a,b,c]))
                   <$> t .:? "addr:street"
@@ -194,13 +200,13 @@ instance FromJSON Restaurant where
 
 
 data Restaurant = Restaurant
-    { id :: Int
-    , lat :: Double
-    , lon :: Double
-    , name :: Text
-    , descr :: Maybe Text
-    , cuisine :: Maybe Text
-    , openingHours :: Maybe Text
-    , addr :: Text
-    , phone :: Maybe Text
+    { restaurantId :: Int
+    , restaurantLat :: Double
+    , restaurantLon :: Double
+    , restaurantName :: Text
+    , restaurantDescr :: Maybe Text
+    , restaurantCuisine :: Maybe [Text]
+    , restaurantOpeningHours :: Maybe Text
+    , restaurantAddr :: Text
+    , restaurantPhone :: Maybe Text
     } deriving Show
